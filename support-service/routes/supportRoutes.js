b5e3f6ece6c4625bf8137761720badea736df6f3
TASSA <hiba.tassa@worldline.com>
2026-04-24 09:33:55 +0200

    support client

---
+++ support-service/routes/supportRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const supportController = require('../controllers/supportController');
+
+// Route pour le formulaire classique
+router.post('/form', supportController.submitContactForm);
+
+// Routes pour le Chatbot
+router.post('/chat/start', supportController.startChatSession);
+router.post('/chat/message', supportController.handleChatMessage);
+
+module.exports = router;
